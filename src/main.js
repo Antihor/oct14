401993d99de0f63908e439dcdbc99d093b8a049b
Antihor <66antigor@gmail.com>
2024-10-14 16:15:39 +0300

    aaa

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const formRef = document.querySelector('.form');
+const buttRef = document.querySelector('button');
+console.log(buttRef);
